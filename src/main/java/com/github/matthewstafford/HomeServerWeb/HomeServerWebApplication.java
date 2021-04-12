101b8820827ecb9fcbfb182fcf72feff749ccb36
mattukdev <mattxo@hotmail.com>
2021-04-12 10:07:03 +0700

    initial

---
+++ src/main/java/com/github/matthewstafford/HomeServerWeb/HomeServerWebApplication.java
@@ -0,0 +1,13 @@
+package com.github.matthewstafford.HomeServerWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeServerWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeServerWebApplication.class, args);
+	}
+
+}
