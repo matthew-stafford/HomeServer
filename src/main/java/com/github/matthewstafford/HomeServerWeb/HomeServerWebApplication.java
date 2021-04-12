27eb9b1c313274728f0bbc59797abcda0e812fa4
mattukdev <mattxo@hotmail.com>
2021-04-12 13:02:04 +0700

    Springboot now working with react embedded. To build run mvn install

--- src/main/java/com/github/matthewstafford/HomeServerWeb/HomeServerWebApplication.java
+++ src/main/java/com/github/matthewstafford/HomeServerWeb/HomeServerWebApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HomeServerWebApplication {
 
+	/*
+	 * Server port can be changed by using -Dserver.port=80 as a param when
+	 * running the jar file
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(HomeServerWebApplication.class, args);
 	}
---
+++ src/main/js/app.js
@@ -0,0 +1,21 @@
+const React = require('react'); 
+const ReactDOM = require('react-dom'); 
+
+class App extends React.Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+
+	render() {
+		return (
+			<div><p>hello</p></div>
+		)
+	}
+}
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('react')
+)
