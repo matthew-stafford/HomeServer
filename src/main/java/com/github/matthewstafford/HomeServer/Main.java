428b196cdf7ce4de3e021f7fd982f0ad8a5e33e3
matthew-stafford <matthew.stafford@live.co.uk>
2021-04-13 11:39:17 +0700

    refactor to tidy project structure and rename from HomeServerWeb to just HomeServer

--- src/main/java/com/github/matthewstafford/HomeServer/Main.java
+++ src/main/java/com/github/matthewstafford/HomeServer/Main.java
@@ -1,17 +1,17 @@
-package com.github.matthewstafford.HomeServerWeb;
+package com.github.matthewstafford.HomeServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HomeServerWebApplication {
+public class Main {
 
 	/*
 	 * Server port can be changed by using -Dserver.port=80 as a param when
 	 * running the jar file
 	 */
 	public static void main(String[] args) {
-		SpringApplication.run(HomeServerWebApplication.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 
 }
--- src/main/java/com/github/matthewstafford/HomeServer/controllers/HomeController.java
+++ src/main/java/com/github/matthewstafford/HomeServer/controllers/HomeController.java
@@ -1,4 +1,4 @@
-package com.github.matthewstafford.HomeServerWeb.controllers;
+package com.github.matthewstafford.HomeServer.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
